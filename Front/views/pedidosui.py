af6e95852abcf9576c4e8d09f6dbce89bf3ca5d0
Rodrigo García <rg.psiac@gmail.com>
2026-08-19 00:44:49 -0600

    Mejora UX/UI

--- Front/views/pedidosui.py
+++ Front/views/pedidosui.py
@@ -5,40 +5,41 @@ import plotly.express as px
 
 st.title("Gestión de Pedidos")
 cliente_api = APIClient()
-ordenes = cliente_api.obtener_ordenes()
-
+with st.spinner("Obteniendo Pedidos..."):
+    ordenes = cliente_api.obtener_ordenes()
 
 if len(ordenes) == 0:
     st.info("No hay órdenes registradas en el sistema")
 else:
 
-    df = pd.DataFrame(ordenes)
-
-    columnas_editor_ordenes = ["Id Orden", "Id Cliente", "Cliente", "Teléfono", "Tipo de Pedido", "Estatus", "Fecha de Entrega", "Total", "Pagado", "Deuda"]
-
-    df_ordenes = df[columnas_editor_ordenes].drop_duplicates(subset="Id Orden").reset_index(drop=True)
-    if "Explorar" not in df_ordenes.columns:
-        df_ordenes.insert(0,"Explorar",False)
-
     st.subheader("Registro de Pedidos")
-    df_editado = st.data_editor(
-        data=df_ordenes,
-        use_container_width=True,
-        hide_index=True,
-        key="editor_ordenes",
-        column_config={
-            "Id Orden": st.column_config.NumberColumn("ID"),
-            "Cliente": st.column_config.TextColumn("Nombre del cliente", disabled=False),
-            "Teléfono": st.column_config.NumberColumn("Celular"),
-            "Tipo de Pedido": st.column_config.TextColumn("Tipo de Pedido"),
-            "Total": st.column_config.NumberColumn("Precio"),
-            "Fecha de Entrega": st.column_config.SelectboxColumn("Fecha de Entrega",disabled=False, help="Elige la fecha por la que quieras actualizar el pedido", options=["Lunes 24 de agosto", "Viernes 28 de agosto", "Lunes 31 de agosto", "Viernes 4 de septiembre", "Lunes 7 de septiembre", "Viernes 11 de septiembre"]),
-            "Pagado": st.column_config.NumberColumn("Pagado", format="$%d", min_value=0, disabled=False),
-            "Deuda": st.column_config.NumberColumn("Deuda", format="$%d", min_value=0),
-            "Estatus": st.column_config.SelectboxColumn("Estatus", help="Cambia el Estatus según el avance del pedido", required=True, options=["Pendiente", "En proceso", "Empacado", "Entregado", "Devuelto por cambios"]),
-            "Explorar": st.column_config.CheckboxColumn(label="Ver detalles",width='small', pinned=True, default=False),
-            "Id Cliente": None
-                },
+    with st.spinner("Cargando tabla de registros..."):
+        df = pd.DataFrame(ordenes)
+
+        columnas_editor_ordenes = ["Id Orden", "Id Cliente", "Cliente", "Teléfono", "Tipo de Pedido", "Estatus", "Fecha de Entrega", "Total", "Pagado", "Deuda"]
+
+        df_ordenes = df[columnas_editor_ordenes].drop_duplicates(subset="Id Orden").reset_index(drop=True)
+        if "Explorar" not in df_ordenes.columns:
+            df_ordenes.insert(0,"Explorar",False)
+        df_ordenes = df_ordenes.set_index("Id Orden", drop=False)
+
+        df_editado = st.data_editor(
+            data=df_ordenes,
+            use_container_width=True,
+            hide_index=True,
+            key="editor_ordenes",
+            column_config={
+                "Id Orden": st.column_config.NumberColumn("ID"),
+                "Cliente": st.column_config.TextColumn("Nombre del cliente", disabled=False),
+                "Teléfono": st.column_config.NumberColumn("Celular"),
+                "Tipo de Pedido": st.column_config.TextColumn("Tipo de Pedido"),
+                "Total": st.column_config.NumberColumn("Precio"),
+                "Fecha de Entrega": st.column_config.SelectboxColumn("Fecha de Entrega",disabled=False, help="Elige la fecha por la que quieras actualizar el pedido", options=["Lunes 24 de agosto", "Viernes 28 de agosto", "Lunes 31 de agosto", "Viernes 4 de septiembre", "Lunes 7 de septiembre", "Viernes 11 de septiembre"]),
+                "Pagado": st.column_config.NumberColumn("Pagado", format="$%d", min_value=0, disabled=False),
+                "Deuda": st.column_config.NumberColumn("Deuda", format="$%d", min_value=0),
+                "Estatus": st.column_config.SelectboxColumn("Estatus", help="Cambia el Estatus según el avance del pedido", required=True, options=["Pendiente", "En proceso", "Empacado", "Entregado", "Devuelto por cambios"]),
+                "Explorar": st.column_config.CheckboxColumn(label="Ver detalles",width='small', pinned=True, default=False),
+                "Id Cliente": None},
                 disabled=["Id Orden","Teléfono","Tipo de Pedido","Total","Deuda", "Id Cliente"])
 
     if st.button("Guardar Cambios"):
@@ -46,21 +47,22 @@ else:
         if not cambios:
             st.warning("No hay cambios para guardar")
         else:
-            for idx_fila, cambio in cambios.items():
-                id_orden = df_ordenes.iloc[idx_fila]["Id Orden"]
-                if "Estatus" in cambio:
-                    nuevo_estado = cambio["Estatus"]
-                    cliente_api.actualizar_estado(id_orden=id_orden, nuevo_estado=nuevo_estado)
-                if "Cliente" in cambio:
-                    nuevo_nombre = cambio["Cliente"]
-                    cliente_api.actualizar_nombre(id_orden=id_orden, nuevo_nombre=nuevo_nombre)
-                if "Fecha de Entrega" in cambio:
-                    nueva_fecha = cambio["Fecha de Entrega"]
-                    cliente_api.actualizar_fecha_entrega(id_orden=id_orden, nueva_fecha=nueva_fecha)
-                if "Pagado" in cambio:
-                    pago = cambio["Pagado"]
-                    cliente_api.actualizar_pago(id_orden=id_orden, pago=pago)
-            st.success("Cambios hechos correctamente")
+            with st.spinner("Guardando cambios..."):
+                for idx_fila, cambio in cambios.items():
+                    id_orden = idx_fila
+                    if "Estatus" in cambio:
+                        nuevo_estado = cambio["Estatus"]
+                        cliente_api.actualizar_estado(id_orden=id_orden, nuevo_estado=nuevo_estado)
+                    if "Cliente" in cambio:
+                        nuevo_nombre = cambio["Cliente"]
+                        cliente_api.actualizar_nombre(id_orden=id_orden, nuevo_nombre=nuevo_nombre)
+                    if "Fecha de Entrega" in cambio:
+                        nueva_fecha = cambio["Fecha de Entrega"]
+                        cliente_api.actualizar_fecha_entrega(id_orden=id_orden, nueva_fecha=nueva_fecha)
+                    if "Pagado" in cambio:
+                        pago = cambio["Pagado"]
+                        cliente_api.actualizar_pago(id_orden=id_orden, pago=pago)
+            st.toast("Cambios hechos correctamente")
             st.rerun()
 
     st.divider()
@@ -72,7 +74,8 @@ else:
         columnas_editor_detalles = ["Id Orden", "Id detalles", "Producto", "Detalle", "Cantidad", "Pertenencia"]
 
         df_detalles = df[df["Id Orden"] == id_selected]
-        df_detalles = df_detalles[columnas_editor_detalles].reset_index(drop=True)
+        df_detalles = df_detalles[columnas_editor_detalles]
+        df_detalles = df_detalles.set_index("Id detalles", drop=False)
         opciones_disponbiles = df["Detalle"].unique().tolist()
         st.subheader(f"Detalles de la orden {id_selected}")
 
@@ -108,16 +111,17 @@ else:
             if not cambios:
                 st.warning("No hay cambios por guardar")
             else:
-                for idx_fila, cambio in cambios.items():
-                    id_detalle_selected = df_detalles.iloc[idx_fila]["Id detalles"]
-                    n_detalle = cambio.get("Detalle")
-                    n_cantidad = cambio.get("Cantidad")
-                    cliente_api.actualizar_detalle_orden(
-                        id_detalle=id_detalle_selected,
-                        nuevo_detalle=n_detalle,
-                        nueva_cantidad=n_cantidad
-                    )
-                st.success("Cambios hechos correctamente")
+                with st.spinner("Guardando cambios..."):
+                    for idx_fila, cambio in cambios.items():
+                        id_detalle_selected = idx_fila
+                        n_detalle = cambio.get("Detalle")
+                        n_cantidad = cambio.get("Cantidad")
+                        cliente_api.actualizar_detalle_orden(
+                            id_detalle=id_detalle_selected,
+                            nuevo_detalle=n_detalle,
+                            nueva_cantidad=n_cantidad
+                        )
+                st.toast("Cambios hechos correctamente")
                 st.rerun()
 
     else:
