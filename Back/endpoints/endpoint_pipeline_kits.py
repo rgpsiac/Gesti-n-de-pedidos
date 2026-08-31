7ccdfa826592413f8959e58a3201913cb79ad1d9
Rodrigo García <rg.psiac@gmail.com>
2026-08-30 21:51:30 -0600

    C

--- Back/endpoints/endpoint_pipeline_kits.py
+++ Back/endpoints/endpoint_pipeline_kits.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from Back.repositories.bd import get_db
 from Back.models.dtos_tipob import DTOBSheets
 from Back.orquestadores.orquestador_kits import OrquestadorPipelineKits
+from Back.orquestadores.orquestador_stock import OrquestadorStock
 from fastapi import HTTPException, status
 from Back.utils.api_verification import verificar_api
 
@@ -14,6 +15,8 @@ def recibir_webhook_kits(payload: DTOBSheets, db: Session = Depends(get_db)):
     try:
         orquestador = OrquestadorPipelineKits(db=db)
         orquestador.procesar_pedido(payload)
+        orquestador_stock = OrquestadorStock(db=db)
+        orquestador_stock.asignar_items()
         return {"mensaje":"Pedido guardado"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error procesando el webhook: {str(e)}")
